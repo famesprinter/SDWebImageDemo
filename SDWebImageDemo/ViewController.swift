129481e44695ac36150e814ac865da04ff46cdcc
Kittitat Rodphotong <kittitat_rpt@hotmail.com>
2016-10-11 22:01:24 +0700

    - Initial Project.

---
+++ SDWebImageDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SDWebImageDemo
+//
+//  Created by Kittitat Rodphotong on 10/11/2559 BE.
+//  Copyright © 2559 FameSprinter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
